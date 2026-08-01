ed8833f974078cad09f61ef1cd3959aa3ce056cf
Surafel Melliyon <surafelmelliyon111@gmail.com>
2026-08-01 19:31:54 +0300

    Implement  client paeg structure

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,22 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+
+      <p>Welcome to the Home page!</p>
+      <ul>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+        <li>
+          <a href="/register">Register</a>
+        </li>
+        <li></li>
+          <a href="/dashboard">Dashboard</a>
+        </li>
+      </ul>
+    </div>
   )
 }
 
