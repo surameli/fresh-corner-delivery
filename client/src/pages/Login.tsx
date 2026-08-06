dcb34d6efb64808c0518e36b968b8dd77f02e97d
Surafel Melliyon <surafelmelliyon111@gmail.com>
2026-08-06 23:45:10 +0300

    Implement login in the left side

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,8 +1,35 @@
+import { useState } from "react"
+import { heroSectionData } from "../assets/assets"
 
 
 const Login = () => {
+
+   const [isLoginstate, setLoginstate] = useState(true)
+   const[name, setName] = useState("")
+   const [email, setEmail] = useState("")
+   const[password, setPassword] = useState("")
+   const [loading, setLoading] = useState(false)
+
+   const handleSubmit = async (e: React.SubmitEvent) => {
+    e.preventDefault()
+    setLoading(true)
+    setTimeout(() => window.location.href = "/", 1000)
+   }
   return (
-    <div>Login</div>
+    <div className="flex min-h-screen">
+      {/* left side  */}
+      <div className="hidden lg:flex lg:w-1/2 bg-app-green relative items-center justofy-center">
+           <img src = {heroSectionData.hero_image} alt = "" className="absolute insert-0 h-full object-cover bg-center opacity-10" />  
+        <div className="relative text-center px-12">
+          <h2 className="text-4xl font-semibold text-white mb-4">Welcome back to Fresh Corner!</h2>
+          <p className="text-white/60 font-serif text-xl max-w-sm mx-auto">fresh and delicious groceries delivered to your door</p>
+        </div>
+      </div>
+          {/* right side  */}
+        
+
+
+    </div>
   )
 }
 
