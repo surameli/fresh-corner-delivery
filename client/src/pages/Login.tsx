7bc279c36630180beca66c2c41d438a8e6258132
Surafel Melliyon <surafelmelliyon111@gmail.com>
2026-08-07 12:31:38 +0300

    Implement full login and signup page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { heroSectionData } from "../assets/assets"
+import { Link} from "react-router-dom"
+import { BikeIcon, Loader2Icon, LockIcon, MailIcon, UserIcon } from "lucide-react"
 
 
 const Login = () => {
@@ -21,12 +23,72 @@ const Login = () => {
       <div className="hidden lg:flex lg:w-1/2 bg-app-green relative items-center justofy-center">
            <img src = {heroSectionData.hero_image} alt = "" className="absolute insert-0 h-full object-cover bg-center opacity-10" />  
         <div className="relative text-center px-12">
-          <h2 className="text-4xl font-semibold text-white mb-4">Welcome back to Fresh Corner!</h2>
+          <h2 className="text-4xl font-semibold text-white mb-4">Welcome back to Fresh Delivery!</h2>
           <p className="text-white/60 font-serif text-xl max-w-sm mx-auto">fresh and delicious groceries delivered to your door</p>
         </div>
       </div>
           {/* right side  */}
-        
+        <div className="flex-1 flex-center px-4 py-12 bg-app-cream">
+          <div className="w-full max-w-md">
+            {/* form header message */}
+            <div className="text-center mb-8">
+              <Link to= "/" className="inline-flex items-center gap-2 mb-6">
+               <BikeIcon className="size-8 text-app-green"/>
+               <span className="text-app-green text-2xl font-semibold">Fresh Delivery</span>
+              </Link>
+                <h1 className="text-2xl font-semibold text-app-green mb-2">
+                  {isLoginstate ? "Sign in to your account" : "sign up for a new account"}
+                </h1>
+                <p className="text-sm text-app-text-light">
+                  {isLoginstate ? "Don't have an account?" : "Already have an account?"}
+                  <button  onClick={() => setLoginstate(!isLoginstate)} className=" font-semibold ml-1 text-orange-500 hover:text-orange-600 transition-colors">
+                    {isLoginstate ? "Sign up" : "Sign in"}</button>
+                </p>
+              
+              
+            </div>
+            {/* login register form */}
+            <form onSubmit={handleSubmit} className="space-y-5">
+              {!isLoginstate && (
+                  <label className="text-sm flex flex-col gap-1">
+                    Name
+                    <div className="relative">
+                        <UserIcon className="absolute left-3.5 top-1/2 -translate-y-1/2 size-4 text-app-text-light" />
+                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} required placeholder="Your Name" 
+                        className="w-full pl-11 pr-4 py-3 text-sm bg-white rounded-xl border not-focus:border-app-border transition-all"  />
+                    </div>
+                    </label>
+
+                    
+                  
+                
+              )}
+              <label className="text-sm flex flex-col gap-1">
+                    Email Address
+                    <div className="relative">
+                        <MailIcon className="absolute left-3.5 top-1/2 -translate-y-1/2 size-4 text-app-text-light" />
+                        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} required placeholder="Your@gmail.com" 
+                        className="w-full pl-11 pr-4 py-3 text-sm bg-white rounded-xl border not-focus:border-app-border transition-all"  />
+                    </div>
+                    </label>
+
+                    <label className="text-sm flex flex-col gap-1">
+                    Password
+                    <div className="relative">
+                        <LockIcon className="absolute left-3.5 top-1/2 -translate-y-1/2 size-4 text-app-text-light" />
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required placeholder="......." 
+                        className="w-full pl-11 pr-4 py-3 text-sm bg-white rounded-xl border not-focus:border-app-border transition-all"  />
+                    </div>
+                    </label>
+                    <button type="submit"  disabled={loading} className="flex-center w-full py-3 bg-green-950 text-white fonst-semibold rounded-xl hover:bg-green-900 transition-colors disabled:opacity-50">
+                    {loading ? <Loader2Icon className="animate-spin"/> : isLoginstate ? "Sign In" : "Sign Up"}
+                    </button>
+              
+            </form>
+
+          </div>
+
+        </div>
 
 
     </div>
