ed2dd2316651d8c84ec6419aa52cd8541cf6c47d
Surafel Melliyon <surafelmelliyon111@gmail.com>
2026-08-26 20:59:14 +0300

    add buttons and descriptionon prompbanner file

--- client/src/components/AppPromoBanner.tsx
+++ client/src/components/AppPromoBanner.tsx
@@ -1,12 +1,17 @@
+import { appPromoBannerData } from "../assets/assets"
 
 
 const AppPromoBanner = () => {
   return (
     <section className="max-w-7xlmx-auto px-4 sm:px-6 lg:px-8 py-20 my-14 bg-green-950 rounded-2xl">
       {/* left side content */}
-     <div>
+     <div className="flex flex-col md:flex-row litems-center justify-between gap-8 xl:px-10 ">
+        <div className="text-center md:text-left">
+          <h2>{appPromoBannerData.title}</h2>
+          <p>{appPromoBannerData.description}</p>
+          
+        </div>
 
-      
      </div>
        {/* right side content */}
     </section>
