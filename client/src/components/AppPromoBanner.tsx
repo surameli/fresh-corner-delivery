3b041d9b613bf6816807e8117dd9384392791c3c
Surafel Melliyon <surafelmelliyon111@gmail.com>
2026-08-26 20:54:29 +0300

    add promobanner structure

--- client/src/components/AppPromoBanner.tsx
+++ client/src/components/AppPromoBanner.tsx
@@ -2,7 +2,14 @@
 
 const AppPromoBanner = () => {
   return (
-    <div>AppPromoBanner</div>
+    <section className="max-w-7xlmx-auto px-4 sm:px-6 lg:px-8 py-20 my-14 bg-green-950 rounded-2xl">
+      {/* left side content */}
+     <div>
+
+      
+     </div>
+       {/* right side content */}
+    </section>
   )
 }
 
