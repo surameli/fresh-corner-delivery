fc6eadcfaca74929597ae287807d811cfa37cd27
Surafel Melliyon <surafelmelliyon111@gmail.com>
2026-08-27 12:36:20 +0300

    create cartsidebar file

--- client/src/components/CartSidebar.tsx
+++ client/src/components/CartSidebar.tsx
@@ -2,7 +2,16 @@
 
 const CartSidebar = () => {
   return (
-    <div>CartSidebar</div>
+    <>
+    {/* overlay */}
+      <div className="fixed inset-0 bg-black/40 z-50 transition-opacity" />
+
+      {/* sidebar */}
+      <div className="fixed right-0 top-0 h-full w-full max-w-md bg-white z-50 shadow-2xl flex flex-col animate-slide-in-right">
+        
+
+      </div>
+    </>
   )
 }
 
