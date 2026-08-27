a56fe1f68acffdcd2de0344b474922a7540e6c16
Surafel Melliyon <surafelmelliyon111@gmail.com>
2026-08-27 09:34:40 +0300

    add input and buttons on the newsletter

--- client/src/components/Home/Newsletter.tsx
+++ client/src/components/Home/Newsletter.tsx
@@ -8,6 +8,22 @@ const Newsletter = () => {
             <div className="size-16 bg-white rounded-xl flex-center mx-auto mb-6 shadow">
                 <MailIcon className="size-8 text-app-green" strokeWidth={1.5}/>
             </div>
+            <h2 className="text-3xl font-semibold text-app-green mb-4"> Subscribe to our Newsletter</h2>
+            <p className="text-app-text-light mb-8 text-base">Get weekly updates on
+                fresh produce, seasonal offers, and exclusive discounts right to your
+                inbox.</p>
+
+            <form onSubmit={(e)=>e.preventDefault()}
+             className="flex flex-col sm:flex-row gap-3 max-w-md mx-auto">
+              <input type="email" placeholder="Enter your email address" required
+               className="flex-1 px-5 py-3.5 rounded-xl border border-app-border
+             focus:border-app-green focus:ring bg-white text-sm transition-all"/>
+
+             <button type="submit" className="px-8 py-3.5 bg-app-green
+             text-white font-semibold rounded-xl hover:bg-app-green-light
+             transition-colors shadow-sm whitespace-nowrap active: scale-[0.98]
+             " >Subscribe</button>
+               </form>    
 
         </div>
     </section>
