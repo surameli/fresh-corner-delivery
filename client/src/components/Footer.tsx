76f4157298c3490cf41abfa80658dbe1489d0685
Surafel Melliyon <surafelmelliyon111@gmail.com>
2026-08-27 09:43:03 +0300

    create footers file on componenets

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,20 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-app-green text-white">
+
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+            {/* --top-- */}
+            <div>
+
+            </div>
+            {/* bottom */}
+        </div>
+
+        
+    </footer>
+  )
+}
+
+export default Footer
