0934fb3f418d61c72f415b6b1d3acab1c591d1f2
Surafel Melliyon <surafelmelliyon111@gmail.com>
2026-08-28 01:10:23 +0300

    fix: product card  import

--- client/src/components/CartSidebar.tsx
+++ client/src/components/CartSidebar.tsx
@@ -1,7 +1,7 @@
 import { ArrowRightIcon, MinusIcon, PlusIcon, ShoppingBagIcon, Trash2Icon, TrashIcon, XIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { useCart } from "../context/CartContext";
-import { UpAndDown } from "@icon-park/react";
+
 
 
 const CartSidebar = () => {
