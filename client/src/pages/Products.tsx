5b498da8a9f2b2a7a036e0bf6cbbe4f1dff4b0e7
surameli <surafelmelliyon111@gmail.com>
2026-09-08 22:02:12 +0300

    implement product.tsx file

--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import type { Product } from "../types";
 
 
 const Products = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [products, setProducts] = useState<Product[]>([]);
+  const [totalpages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [mobileFilterOpen, setMobileFilterOpen] = useState(false);
+
+  const category = searchParams.get("category") || "";
+  const organic = searchParams.get("organic") || "";
+  
   return (
     <div>Products</div>
   )
