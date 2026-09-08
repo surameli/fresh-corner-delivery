50b1318338d2bd3de3a008fc16585755f587b43b
surameli <surafelmelliyon111@gmail.com>
2026-09-09 00:23:00 +0300

    implement product.tsx file

--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import type { Product } from "../types";
+import { dummyProducts } from "../assets/assets";
 
 
 const Products = () => {
@@ -12,7 +13,22 @@ const Products = () => {
 
   const category = searchParams.get("category") || "";
   const organic = searchParams.get("organic") || "";
-  
+  const sort = searchParams.get("sort")  || "";
+  const page = Number(searchParams.get("page")) || 1;
+  const minprice = searchParams.get("minprice") || "";
+  const maxprice = searchParams.get("maxprice") || "";
+
+
+  const fetchProducts = async () => {
+    setLoading(true);
+    setProducts(dummyProducts.filter((p) => p.category === category  || p.category === ""));
+    setLoading(false);
+  };
+
+  useEffect(()=>{
+    fetchProducts()
+  },[category, organic, sort, page, minprice, maxprice])
+
   return (
     <div>Products</div>
   )
