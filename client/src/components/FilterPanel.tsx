86961f31eea14ca4f3a9fb4a00aec519ebd6c87d
surameli <surafelmelliyon111@gmail.com>
2026-09-10 00:30:37 +0300

    implement filter panel on products page for mobile size

--- client/src/components/FilterPanel.tsx
+++ client/src/components/FilterPanel.tsx
@@ -17,7 +17,20 @@ const FilterPanel = ({categories, category , minPrice, maxPrice, updateFilters, 
             </div>
         </div>
 
-      
+      {/* price range */}
+        <div>
+            <h3 className="text-sm font-semibold text-app-green mb-3">Price Range</h3>
+            <div className="flex items-center gap-2">
+                <input type="number" placeholder="Max" value={minPrice || ""} onChange={(e) => updateFilters("minprice", e.target.value)} className="w-full text-sm text-app-text-light placeholder:text-app-text-light/50 px-3 py-2 rounded-lg border bg-white  not-focus:border-app-border"/>
+                <span className="text-app-text-light">-</span>
+                <input type="number" placeholder="Min" value={maxPrice || ""} onChange={(e) => updateFilters("maxprice", e.target.value)} className="w-full text-sm text-app-text-light placeholder:text-app-text-light/50 px-3 py-2 rounded-lg border bg-white  not-focus:border-app-border"/>
+            </div>
+        </div>
+        {hasFilters && (
+        <button onClick={clearFilters} className="w-full text-sm text-app-error font-medium  py-2 rounded-lg  hover:bg-red-50 transition-colors">
+            Clear All Filters
+        </button>
+        )}
     </div>
   )
 }
--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 import type { Product } from "../types";
 import { categoriesData, dummyProducts } from "../assets/assets";
-import { ChevronDown, Home, SlidersHorizontal } from "lucide-react";
+import { ChevronDown, Home, SlidersHorizontal, XIcon } from "lucide-react";
 import ProductCard from "../components/ProductCard";
 import Loading from "../components/loading";
 import FilterPanel from "../components/FilterPanel";
@@ -131,12 +131,29 @@ const Products = () => {
                 ))}
               </div>
             )}
-
            </main>
          </div>
-
       </div>
 
+      {/* mobile filter panel */}
+      {mobileFilterOpen && (
+        <>  
+        <div className="fixed inset-0 bg-black/50 z-50" onClick={() => setMobileFilterOpen(false)}/>
+        <div className="fixed bottom-0 left-0 right-0  bg-white z-50 rounded-t-2xl max-h-[80vh] overflow-y-auto animate-slide-in-up">
+          {/* <FilterPanel categories={categoriesData} category={category} organic={organic} minPrice={minprice} maxPrice={maxprice} updateFilters={updateFilters} clearFilters={clearFilters} hasFilters={hasFilters}/> */}
+          <div className="flex items-center justify-between p-4 border-b border-app-border">
+            <h3 className="text-lg font-semibold text-app-green">Filters</h3>
+            <button onClick={() => setMobileFilterOpen(false)} className="p-2 hover:bg-app-cream rounded-lg">
+                <XIcon className="size-5"/>
+            </button>
+          </div>
+          <div>
+            <FilterPanel categories={categoriesData} category={category} organic={organic} minPrice={minprice} maxPrice={maxprice} updateFilters={updateFilters} clearFilters={clearFilters} hasFilters={hasFilters}/>
+          </div>
+          
+         </div>
+        </>
+      )}
     </div>
   )
 }
