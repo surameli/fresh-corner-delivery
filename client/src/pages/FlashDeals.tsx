6204c77d76c19e47d97fb9710bd2efb869508050
surameli <surafelmelliyon111@gmail.com>
2026-09-10 22:34:08 +0300

    add flashdeals page on my project

--- client/src/pages/FlashDeals.tsx
+++ client/src/pages/FlashDeals.tsx
@@ -2,6 +2,8 @@ import { use, useEffect, useState } from "react"
 import type { Product } from "../types"
 import { dummyProducts } from "../assets/assets"
 import { Zap } from "lucide-react"
+import Loading from "../components/loading"
+import ProductCard from "../components/ProductCard"
 
 const FlashDeals = () => {
 
@@ -26,10 +28,27 @@ const FlashDeals = () => {
             <Zap className="size-6 fill-white"/>
           </div>
           <p className=" text-white/80 max-w-md mx-auto">Limited time offers! Grab the best deals on fresh groceries before they run out!</p>
-
-        </div>
-
-      </div>
+         </div>
+         </div>
+           <div className="max-w-7x1 mx-auto px-4 sm:px-6 1g:px-8 py-8">
+            {loading ? ( <Loading />) : (
+                products.length === 0 ? (
+             <div className="text-center py-16">
+            <Zap className="size-16 text-app-border mx-auto mb-4" />
+            <h2 className="text-lg font-semibold text-app-green mb-2">No
+              deals right now</h2>
+            <p className="text-sm text-app-text-light">Check back soon
+              for amazing offers!</p>
+           </div>
+           ) : (
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-4">
+            {products.map((product) => product.stock > 0 && (
+              <ProductCard key={product._id} product={product} />
+            ))}
+         </div>
+        )
+     )}
+</div>   
 
     </div>
   )
