77beb2df93a62ba85ebab15826adbc7476e6ed2a
surameli <surafelmelliyon111@gmail.com>
2026-09-10 23:10:26 +0300

    implement product details page

--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -1,6 +1,43 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+import type { Product } from "../types";
+import { useEffect, useState } from "react";
+import { dummyProducts } from "../assets/assets";
+import Loading from "../components/loading";
 
 
 const ProductPage = () => {
+  const currency = import.meta.env.VITE_CURRENCY_SYMBOL || "Birr";
+  const {id} = useParams()
+  const navigate = useNavigate()
+  const {items , addToCart,updateQuantity ,removeFromCart} = useCart()
+
+  const [product, setproduct] = useState<Product | null>(null)
+  const [relatedProducts, setRelatedProducts] = useState<Product[]>([])
+  const [loading, setLoading] = useState(true)
+  const [localQuantity, setLocalQuantity] = useState(1)
+
+
+  useEffect(() => {
+    setLoading(true)
+    setLocalQuantity(1)
+    window.scrollTo(0, 0)
+    const productInCart = dummyProducts.find((p) => p._id === id);
+    setproduct (product!)
+    setRelatedProducts(dummyProducts.filter((p) => p._id !== id));
+    setLoading(false)
+  },[id , navigate])
+
+  if(loading) return <Loading/> 
+  if(!product) return  null;
+
+
+  const cartItem = items.find((item) => item.product._id === product._id);
+  const incart = !! cartItem;
+  const displayQuantity = incart ? cartItem!.quantity : localQuantity;
+
+
+
   return (
     <div>ProductPage</div>
   )
