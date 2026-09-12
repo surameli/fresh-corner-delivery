4abd78c9071233dfaee2c21d97fdb63b1b89c076
surameli <surafelmelliyon111@gmail.com>
2026-09-12 18:20:23 +0300

    fix flashdeals page

--- client/src/pages/FlashDeals.tsx
+++ client/src/pages/FlashDeals.tsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react"
+import {  useEffect, useState } from "react"
 import type { Product } from "../types"
 import { dummyProducts } from "../assets/assets"
 import { Zap } from "lucide-react"
