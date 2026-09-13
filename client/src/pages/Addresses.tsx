b1b69ebbfba9b5f1b132d55b6c8135a9edc9e775
surameli <surafelmelliyon111@gmail.com>
2026-09-13 23:40:17 +0300

    implement address page

--- client/src/pages/Addresses.tsx
+++ client/src/pages/Addresses.tsx
@@ -1,6 +1,8 @@
+import { MapPinIcon, PlusIcon } from "lucide-react"
 import { dummyAddressData } from "../assets/assets"
 import type { Address } from "../types"
 import { useEffect, useState } from "react"
+import Loading from "../components/loading"
 
 
 const Addresses = () => {
@@ -35,7 +37,34 @@ const Addresses = () => {
 
     <div className="min-h-screen bg-app-cream">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        
+        {/* page header */}
+
+        <div className="flex items-center justify-between mb-8">
+          <h1 className="text-2xl font-semibold text-app-green">My Addresses</h1>
+          <button onClick={()=>{resetForm(); setShowForm(true)}} 
+          className="px-4 py-2 bg-app-green text-white text-sm font-semibold rounded-xl hover:bg-app-green-light transition-colors flex items-center gap-2">
+            <PlusIcon className="size-4"/> Add Address
+          </button>
+        </div>
+         {/* form modal */}
+
+         {/* addresses list */}
+         {
+          loading ?(
+            <Loading/>
+          ): addresses.length === 0 ?(
+            <div className="text-center py-16">
+              <MapPinIcon className="size-16 text-app-border mx-auto mb-4"/>
+              <h2 className="text-lg font-semibold text-app-green mb-2">NO addresses Saved</h2>
+              <p>Add an address for faster checkout</p>
+
+            </div>
+          ):(
+            <div>
+
+            </div>
+          )
+         }
 
       </div>
 
