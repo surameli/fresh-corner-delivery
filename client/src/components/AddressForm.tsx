fc9dd4b2db35c15ebe065fc36cc789aa1e03288a
surameli <surafelmelliyon111@gmail.com>
2026-09-14 01:50:20 +0300

    implement left side address page

--- client/src/components/AddressForm.tsx
+++ client/src/components/AddressForm.tsx
@@ -1,7 +1,8 @@
+
 import { XIcon } from 'lucide-react'
 
 
-const AddressForm = ({resetForm, handleSubmit, form, setform, editingId}: any) => {
+const AddressForm = ({resetForm, handleSubmit, form, setForm, editingId}: any) => {
   return (
     <>
       {/* overlay */}
@@ -19,6 +20,59 @@ const AddressForm = ({resetForm, handleSubmit, form, setform, editingId}: any) =
                 </button>
             </div>
             {/* form input filds */}
+            <div className='space-y-4'>
+               <div>
+                   <label className='block text-sm font-medium text-app-green mb-1.5'>Label</label>
+                   <input type="text"  placeholder="Home , Work, etc." required className='w-full px-4 py-2.5 text-sm rounded-xl border border-app-border focus:border-app-green outline-none'
+                   value = {form.label} onChange={(e)=> setForm({...form, label: e.target.value })}/>
+
+               </div>
+               <div>
+                   <label className='block text-sm font-medium text-app-green mb-1.5'>street Address</label>
+                   <input type="text"  placeholder="Bole , mexico, etc." required className='w-full px-4 py-2.5 text-sm rounded-xl border border-app-border focus:border-app-green outline-none'
+                   value = {form.address} onChange={(e)=> setForm({...form, address: e.target.value })}/>
+
+               </div>
+               <div className='grid grid-cols-2 gap-3'>
+                <div>
+                   <label className='block text-sm font-medium text-app-green mb-1.5'>City</label>
+                   <input type="text"  placeholder="Home , Work, etc." required className='w-full px-4 py-2.5 text-sm rounded-xl border border-app-border focus:border-app-green outline-none'
+                   value = {form.city} onChange={(e)=> setForm({...form, city: e.target.value })}/>
+
+               </div>
+
+                   <div>
+                   <label className='block text-sm font-medium text-app-green mb-1.5'>sub-city</label>
+                   <input type="text"  placeholder="Home , Work, etc." required className='w-full px-4 py-2.5 text-sm rounded-xl border border-app-border focus:border-app-green outline-none'
+                   value = {form.state} onChange={(e)=> setForm({...form, state: e.target.value })}/>
+
+               </div>
+               </div>
+
+               <div className='grid grid-cols-2 gap-2'>
+                 <div>
+                <label className='block text-sm font-medium text-app-green mb-1.5'>ZIP code</label>
+                   <input type="text"  placeholder="Home , Work, etc." required className='w-full px-4 py-2.5 text-sm rounded-xl border border-app-border focus:border-app-green outline-none'
+                   value = {form.zip} onChange={(e)=> setForm({...form, zip: e.target.value })}/>
+               </div>
+               <div className='flex items-end pb-1'>
+                <label className='flex items-center gap-2 cursor-pointer'>
+                    <input type="checkbox" checked = {form.isDefault} onChange={(e)=> setForm({...form, isDefault: e.target.checked})}/>
+                    <span className='text-sm text-app-text'>Set as default</span>
+                </label>
+                  
+               </div>
+
+
+               </div>
+
+            </div>
+
+            {/* submit button */}
+
+            <button type='submit'>
+
+            </button>
 
         </form>
 
