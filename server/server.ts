e1dd4df89f20e561b75487f466897dc39e645b56
surameli <surafelmelliyon111@gmail.com>
2026-09-19 18:48:37 +0300

    implement routes

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from "cors";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -13,6 +14,7 @@ const port = process.env.PORT || 4000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+ app.use('/api/auth' , authRouter)
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
