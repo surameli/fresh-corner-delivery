8751a550e7f4f2cfdf38ba557232656a139309b7
surameli <surafelmelliyon111@gmail.com>
2026-09-22 21:26:33 +0300

    implement order controller to order products

--- server/controller/orderController.ts
+++ server/controller/orderController.ts
@@ -4,6 +4,7 @@
 
 import { Request, Response } from "express";
 import { prisma } from "../config/prisma.js";
+import { timeStamp } from "node:console";
 
 
 
@@ -85,4 +86,96 @@ export const createOrder =  async (req: Request, res: Response)=>{
      }
 
 
+}
+
+// get user's  order
+// get /api/orders
+
+
+export const getUserOrders =  async (req: Request, res: Response)=>{
+   const {status} = req.query;
+    const where: any ={
+        userId: req.user!.id,
+        NOT: [{paymentMethod: "card", ispaid: false}]
+    }
+    if (status && status !== "all") {
+        where.status = status;
+
+    }
+    const orders = await prisma.order.findMany({
+        where,
+        include: {deliveryPartner: {select:{name: true , phone: true}}},
+        orderBy: {createdAt: "desc"}
+    })
+    res.json({orders})
+}
+// get single order
+//get /api/orders/id
+
+export const getorder =  async (req: Request, res: Response)=>{
+   const order = await prisma.order.findFirst({
+    where : {id: req.params.id as string , userId: req.user!.id},
+    include : {deliveryPartner: {select: {name: true, phone: true, avatar: true, vehicleType: true}}}
+   })
+   if (!order) {
+    return res.status(404).json({ message: " order not found"});
+   }
+   res.json({order})
+}
+
+// update order stsus(admin)
+
+//put /api/orders/:id/status
+
+
+export const updateorderstatus =  async (req: Request, res: Response)=>{
+   const {status, note} = req.body;
+   const order = await prisma.order.findUnique({where: {id: req.params.id as string}})
+
+   if(!order){
+    return res.status(404).json({message: " order not found"});
+   }
+
+   const history = (Array.isArray(order.statusHistory)? order.statusHistory : []) as any[]
+    history.push({status, note: note || `order${status.toLowercase()}`,timeStamp: new Date()})
+   
+    const updateorder = await prisma.order.update({
+        where:{id: req.params.id as string},
+        data: {status, statusHistory: history}
+    })
+    res.json({order: updateorder})
+}
+
+// get all(admin)
+//put /api/orders/all
+
+export const getAllorders =  async (req: Request, res: Response)=>{
+   
+    const orders = await prisma.order.findMany({
+        where: {NOT: [{paymentMethod: "card", isPaid: false}]},
+        include: {
+            user: {select: {name: true, email:true}},
+            deliveryPartner: {select:{name: true , phone: true, email: true}}
+        },
+        orderBy: {createdAt: "desc"}
+    })
+    res.json({orders})
+}
+
+
+//get order location
+// get /api/orders/:id/location
+
+export const getorderlocation =  async (req: Request, res: Response)=>{
+   const order  = await prisma.order.findFirst({
+       where : {id: req.params.id as string, userId: req.user!.id},
+       select: {liveLocation: true, status: true}
+
+   }
+   
+   )
+   if (!order) return res.status(404).json({message: " order not found"})
+    res.json({liveLocation: order.liveLocation , status: order.status})
+    
+   
 }
